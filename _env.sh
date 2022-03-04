bfdb9663a1c4e09723d05d4d918ef598f8a7e359
JGWill Gaia.WSL <jgi@jgwill.com>
2022-03-04 08:10:55 -0500

    add:tst

---
+++ _env.sh
@@ -0,0 +1,1 @@
+export dockertag=guillaumeai/server:gupyter
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. _env.sh
+mkdir -p work
+#docker run --name gupyter -p 8888:8888 guillaumeai/server:gupyter
+docker run -v $(pwd)/work:/home/jovyan/work -p 8888:8888 guillaumeai/server:gupyter
+
+#docker run  -p 8888:8888 guillaumeai/server:gupyter
+
